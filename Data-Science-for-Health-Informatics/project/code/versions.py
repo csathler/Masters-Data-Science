f914ba87e687b4b00c55f7695050a2ffcb2798dd
Carlos Sathler <cssathler@gmail.com>
2019-05-08 20:03:24 -0300

    ds for health informatics

---
+++ Data-Science-for-Health-Informatics/project/code/versions.py
@@ -0,0 +1,28 @@
+# scipy
+import scipy as sp
+print('scipy: %s' % sp.__version__)
+# numpy
+import numpy as np
+print('numpy: %s' % np.__version__)
+# matplotlib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+print('matplotlib: %s' % mpl.__version__)
+# pandas
+import pandas as pd
+print('pandas: %s' % pd.__version__)
+# statsmodels
+import statsmodels as stats
+print('statsmodels: %s' % stats.__version__)
+# scikit-learn
+import sklearn as sk
+print('sklearn: %s' % sk.__version__)
+# theano
+import theano 
+print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow as tf
+print('tensorflow: %s' % tf.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
